cdbba3de9925278b405be5f3b2d6bbb7ae97bfee
Ong03G <paolo.ong@obf.ateneo.edu>
2022-09-16 09:31:04 +0800

    added special outcomes to overloaded << operator to wherein denominator is 1 or 0

--- fractionclass.cpp
+++ fractionclass.cpp
@@ -15,6 +15,14 @@ Fraction::Fraction(int num, int den)
 
 std::ostream& operator<<(std::ostream& output, const Fraction& fractionOutput)
 {
-  output<<fractionOutput.numerator<<"/"<<fractionOutput.denominator<<std::endl;   
+  if (fractionOutput.denominator == 1) { // special outcome for if denominator is equal to 1, in which case it can just be expressed as a whole number
+    output << fractionOutput.numerator << std::endl;
+  }
+  else if (fractionOutput.denominator == 0) { //special outcome for if denominator is equal to 0, in which case it returns an undefined value
+    output << "undefined" << std::endl;
+  }
+  else {
+    output << fractionOutput.numerator << "/" << fractionOutput.denominator << std::endl;
+  }
   return output;
 }
